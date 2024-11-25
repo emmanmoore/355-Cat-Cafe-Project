9c5214e6da2cc959ec1ce7357a1ccec5b2426218
KateBits <79615808+KateBits@users.noreply.github.com>
2024-11-25 16:18:06 -0500

    Update query1.php

--- application/query1.php
+++ application/query1.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Agency Info Page</title>
-</head>
-<body>
-<h1>Agency Information Page</h1>
-<!-- display all cats within agency -->
 <?php
 try {
 require('../pdo_connect.php');
@@ -15,14 +6,29 @@ $result = $dbc->query($sql);
 } catch (PDOException $e){
 echo $e->getMessage();
 }
-
-foreach ($result as $name) {
-echo "<tr>";
-echo "<td>".$name['AgencyName']."</td>";
-echo "<td>".$name['CatName']."</td>";
-echo "</tr>";
-}
-
 ?>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Agency Info Page</title>
+</head>
+<body>
+<h2>Cats</h2>
+	<table>
+		<tr>
+			<th>Agency Name</th>
+			<th>Cat Name</th>
+		</tr>
+		<!-- display all cats within agency -->
+		<?php
+		foreach ($result as $name) {
+		echo "<tr>";
+		echo "<td>".$name['AgencyName']."</td>";
+		echo "<td>".$name['CatName']."</td>";
+		echo "</tr>";
+		}
+		?>
+	</table>
+</body>    
 </html>
