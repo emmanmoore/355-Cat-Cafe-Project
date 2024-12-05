2aaf9ea04ad5c097f899d928ffce8a9a799b8af0
KateBits <79615808+KateBits@users.noreply.github.com>
2024-12-05 12:15:20 -0500

    fixed bug

--- application/orderComplete.php
+++ application/orderComplete.php
@@ -1,17 +1,30 @@
 <?php
 ini_set('display_errors', 1);
-	if(isset($_GET['order_id'])) {  
+	if(isset($_GET['order_id']) && isset($_GET['store'])) {  
 		$oid = $_GET['order_id'];
-	try {
+		$store = $_GET['store'];
+	try{
 		require_once('../pdo_connect.php'); 
+		$sql = 'SELECT OrderID FROM CustomerOrder WHERE OrderID = ? AND StoreID = ? AND completed = 0';
+		$stmt = $dbc->prepare($sql);
+		$stmt->bindParam(1, $oid);
+		$stmt->bindParam(2, $store);
+		$stmt->execute();
+	}catch (PDOException $e){
+		echo $e->getMessage();
+	}
+	$affected = $stmt->RowCount();
+	if ($affected == 0){
+		echo "We could not find an order with that ID at this store to complete. Please try again.";
+		exit;
+	}
+	else{
 		$sql = 'CALL markOrderComplete(?)';
 		$stmt = $dbc->prepare($sql);
 		$stmt->bindParam(1, $oid);
 		$stmt->execute();
-	}catch (PDOException $e){
-			echo $e->getMessage();
-	}}
-	else {
+	}		
+	}else {
 		echo "<h2>You have reached this page in error</h2>";
 		exit;
 	}
